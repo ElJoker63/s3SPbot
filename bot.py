2a5c81deafae58386f3f2c909b40c56d7bfdf517
GH0STsama <leonardoduran2004@gmail.com>
2021-08-23 16:31:39 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,16 +14,18 @@ def ftp(update, context):
 <b>Libres de consumo:</b>
 <b>1. </b><a href="http://ftp.udg.co.cu/">FTP Granma</a>
 <b>2. </b><a href="http://ftp.uo.edu.cu/">FTP Santiago</a>   
+<a href=""></a>
 
-Consumo nacional: 1. Software UCLV (https://soft.uclv.edu.cu/) 
-2. Repo UCLV (http://red.uclv.cu/softlib) 
-3. Visuales UCLV (https://visuales.uclv.cu/) 
-4. Android Store UCLV (http://android.uclv.edu.cu/) 
-5. iOS Store UCLV (http://ios.uclv.edu.cu/) 6. 
-La Mochila UCLV (https://mochila.uclv.cu/) 
-7. FTP Isla de la Juventud (http://ftp.uij.edu.cu/) 
-8. Joven Club Descargas (http://download.jovenclub.cu/) 
-9. La Mochila (https://mochila.cubava.cu/)
+<b>Consumo nacional:</b>
+<b>1. </b><a href="https://soft.uclv.edu.cu/">Software UCLV</a>
+<b>2. </b><a href="http://red.uclv.cu/softlib">Repo UCLV</a>
+<b>3. </b><a href="https://visuales.uclv.cu/">Visuales UCLV</a>
+<b>4. </b><a href="http://android.uclv.edu.cu/">Android Store UCLV</a>
+<b>5. </b><a href="http://ios.uclv.edu.cu/">iOS Store UCLV</a>
+<b>6. </b><a href="https://mochila.uclv.cu/">La Mochila UCLV</a> 
+<b>7. </b><a href="http://ftp.uij.edu.cu/">FTP Isla de la Juventud</a>
+<b>8. </b><a href="http://download.jovenclub.cu/">Joven Club Descargas</a>
+<b>9. </b><a href="https://mochila.cubava.cu/">La Mochila</a>
 ''', parse_mode = "html", disable_web_page_preview = True)
 
 updater = Updater(token = BOT_TOKEN, use_context = True)
